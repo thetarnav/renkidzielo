d3be842368b58492ddae19e520b2832725869ba8
Damian Tarnawski <gthetarnav@gmail.com>
2022-07-13 23:49:01 +0200

    add Navigation component and base styles modifies the path aliases

--- README.md
+++ README.md
@@ -3,3 +3,19 @@
 [![Netlify Status](https://api.netlify.com/api/v1/badges/214ef4b9-4d54-4a55-8004-bcd9c35edda3/deploy-status)](https://app.netlify.com/sites/renkidzielo/deploys)
 
 Renkidzieło Website 3.0
+
+## Development
+
+### VSCode Settings
+
+Add these options to your workspace `settings.json` file:
+
+```json
+{
+  "prettier.documentSelectors": ["**/*.astro"],
+  "[astro]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "tailwindCSS.rootFontSize": 14
+}
+```
---
+++ renkidzielo/src/components/navigation/MobileNavOverlay.tsx
@@ -0,0 +1,18 @@
+import { ParentComponent, createSignal } from "solid-js"
+import styles from "./mobileNav.module.css"
+
+const MobileNavOverlay: ParentComponent = props => {
+	const [isOpen, setIsOpen] = createSignal(false)
+	const toggle = () => setIsOpen(p => !p)
+
+	return (
+		<div class={styles["mobile-nav"]} classList={{ open: isOpen() }}>
+			{props.children}
+			{/* TODO: replace by a IconButton once it's done */}
+			<button class={styles["toggle-button"]} onClick={toggle}>
+				toggle
+			</button>
+		</div>
+	)
+}
+export default MobileNavOverlay

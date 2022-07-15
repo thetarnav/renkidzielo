587b935b0c529d1d4871616e82398fcafb5cd90b
Damian Tarnawski <gthetarnav@gmail.com>
2022-07-15 21:14:02 +0200

    add head tags

--- renkidzielo/src/utils/variables.ts
+++ renkidzielo/src/utils/variables.ts
@@ -1,3 +1,5 @@
 export const ISTAGRAM_URL = "https://www.instagram.com/thea_simulakra"
 export const ALLEGRO_URL = "https://allegro.pl/uzytkownik/ren2er"
 export const ETSY_URL = "https://www.etsy.com/pl/shop/RenkiDzielo"
+export const MAIN_SHOP_URL = ALLEGRO_URL
+export const BASE_URL = "https://renkidzielo.netlify.app"

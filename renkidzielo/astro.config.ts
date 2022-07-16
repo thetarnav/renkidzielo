c1b0242dd7e75c3dfdb07f8bb7e882dfa0efb519
Damian Tarnawski <gthetarnav@gmail.com>
2022-07-16 14:48:30 +0200

    add Icon component

--- renkidzielo/astro.config.ts
+++ renkidzielo/astro.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config"
 import solid from "@astrojs/solid-js"
 import tailwind from "@astrojs/tailwind"
+import Icons from "unplugin-icons/vite"
+import { FileSystemIconLoader } from "unplugin-icons/loaders"
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,6 +13,14 @@ export default defineConfig({
 		}),
 	],
 	vite: {
+		plugins: [
+			Icons({
+				compiler: "solid",
+				customCollections: {
+					assets: FileSystemIconLoader("./assets/icons"),
+				},
+			}),
+		],
 		ssr: {
 			// for ssr support
 			// see https://github.com/solidjs-community/solid-primitives/tree/main/packages/props#ssr-support

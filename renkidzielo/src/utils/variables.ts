d0306ca502507b58582e8bd608bb5c06a9026fb9
Damian Tarnawski <gthetarnav@gmail.com>
2022-07-18 14:21:14 +0200

    highlights section and a slider

--- renkidzielo/src/utils/variables.ts
+++ renkidzielo/src/utils/variables.ts
@@ -3,3 +3,5 @@ export const ALLEGRO_URL = "https://allegro.pl/uzytkownik/ren2er"
 export const ETSY_URL = "https://www.etsy.com/pl/shop/RenkiDzielo"
 export const MAIN_SHOP_URL = ALLEGRO_URL
 export const BASE_URL = "https://renkidzielo.netlify.app"
+
+export const BACKEND_ENDPOINT = "https://my-json-server.typicode.com/thetarnav/renkidzielo"
